d0bda83c709b915e209e84b4911a978f46b147a9
Crypto NIG <cryptomonnaie.nig@gmail.com>
2024-08-07 12:35:22 +0200

    update of testing scripts paths

--- testing_scripts/back-up_values.bat
+++ testing_scripts/back-up_values.bat
@@ -1,5 +1,5 @@
 ECHO OFF
 ECHO Back-up of values
-xcopy /y C:\Users\davidlio\source\nig\node1\src\common\values.py C:\Users\davidlio\source\nig\values_temp\node1
-xcopy /y C:\Users\davidlio\source\nig\node2\src\common\values.py C:\Users\davidlio\source\nig\values_temp\node2
+xcopy /y C:\....path to source folder...\nig\node1\src\common\values.py C:\....path to source folder...\nig\values_temp\node1
+xcopy /y C:\....path to source folder...\nig\node2\src\common\values.py C:\....path to source folder...\nig\values_temp\node2
 PAUSE
--- testing_scripts/copy_env_WO_values.bat
+++ testing_scripts/copy_env_WO_values.bat
@@ -1,20 +1,20 @@
 ECHO OFF
 ECHO Replication of environment
-break>C:\Users\davidlio\source\nig\node3\files\BLOCKCHAIN_DIR.txt
-break>C:\Users\davidlio\source\nig\node3\files\MEMPOOL_DIR.txt
-break>C:\Users\davidlio\source\nig\node3\files\LEADER_NODE_SCHEDULE_DIR.txt
-break>C:\Users\davidlio\source\nig\node3\files\KNOWN_NODES_DIR.txt
-break>C:\Users\davidlio\source\nig\node3\files\MASTER_STATE_DIR.txt
-rmdir /s /q "C:\Users\davidlio\source\nig\node1\STORAGE\"
-MD "C:\Users\davidlio\source\nig\node1\STORAGE\"
-rmdir /s /q "C:\Users\davidlio\source\nig\node2\STORAGE\"
-MD "C:\Users\davidlio\source\nig\node2\STORAGE\"
-rmdir /s /q "C:\Users\davidlio\source\nig\node3\STORAGE\"
-MD "C:\Users\davidlio\source\nig\node3\STORAGE\"
-rmdir /S /q "C:\Users\davidlio\source\nig\node1\src\"
-rmdir /S /q "C:\Users\davidlio\source\nig\node2\src\"
-xcopy /s /y  C:\Users\davidlio\source\nig\node3\src\ C:\Users\davidlio\source\nig\node2\src\
-xcopy /s /y C:\Users\davidlio\source\nig\node3\src\ C:\Users\davidlio\source\nig\node1\src\
-xcopy /s /y C:\Users\davidlio\source\nig\node3\files\ C:\Users\davidlio\source\nig\node2\files\
-xcopy /s /y C:\Users\davidlio\source\nig\node3\files\ C:\Users\davidlio\source\nig\node1\files\
+break>C:\....path to source folder...\nig\node3\files\BLOCKCHAIN_DIR.txt
+break>C:\....path to source folder...\nig\node3\files\MEMPOOL_DIR.txt
+break>C:\....path to source folder...\nig\node3\files\LEADER_NODE_SCHEDULE_DIR.txt
+break>C:\....path to source folder...\nig\node3\files\KNOWN_NODES_DIR.txt
+break>C:\....path to source folder...\nig\node3\files\MASTER_STATE_DIR.txt
+rmdir /s /q "C:\....path to source folder...\nig\node1\STORAGE\"
+MD "C:\....path to source folder...\nig\node1\STORAGE\"
+rmdir /s /q "C:\....path to source folder...\nig\node2\STORAGE\"
+MD "C:\....path to source folder...\nig\node2\STORAGE\"
+rmdir /s /q "C:\....path to source folder...\nig\node3\STORAGE\"
+MD "C:\....path to source folder...\nig\node3\STORAGE\"
+rmdir /S /q "C:\....path to source folder...\nig\node1\src\"
+rmdir /S /q "C:\....path to source folder...\nig\node2\src\"
+xcopy /s /y  C:\....path to source folder...\nig\node3\src\ C:\....path to source folder...\nig\node2\src\
+xcopy /s /y C:\....path to source folder...\nig\node3\src\ C:\....path to source folder...\nig\node1\src\
+xcopy /s /y C:\....path to source folder...\nig\node3\files\ C:\....path to source folder...\nig\node2\files\
+xcopy /s /y C:\....path to source folder...\nig\node3\files\ C:\....path to source folder...\nig\node1\files\
 PAUSE
